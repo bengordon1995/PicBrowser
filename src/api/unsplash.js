192de971ff7fb5d164ce2e1ae16972a9ac15688d
Unknown <bengordon1995@gmail.com>
2019-10-15 10:57:53 -0700

    push

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,9 +1,13 @@
+/*
+    File for externalizing unsplash authorizations
+*/
+
 import axios from 'axios';
 
 
 //creates a customized copy of axios for our requests
 export default axios.create({
-    baseURL:'https://api.unsplash.com';
+    baseURL:'https://api.unsplash.com',
     headers: {
         Authorization: 'Client-ID 6ce100efa37372314315ddfe8543a1a1a0fe0e2f27d63281780661392beba6ed'
     },
